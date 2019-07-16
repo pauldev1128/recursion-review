2e7eb5e3fa50375a2a2f2a7c7a4ef29f42dc5e07
paul cho <pauldev1128@gmail.com>
2019-07-15 17:33:27 -0700

    made changes

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,50 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  let value = obj;
+  let arr = [];
+  // check if value is string then add "" to the string
+  if(typeof value === "string"){
+    return `"` + value + `"`;
+  }
+  // check if value/boolean/null is number then change value to string
+  if(typeof value === "number" || typeof value === "boolean" || value === null){
+    return "" + value;
+  }
+  // if value is a function return undefined
+  if(typeof value === "function"){
+    return undefined;
+  }
+  // if value is null return undefined
+  if(value === undefined){
+    return String(null);
+  }
+  // check if is array
+  if(Array.isArray(value)){  
+    // check if array is empty
+    if(value.length === 0){
+      return "[]"
+    }
+    for(let i = 0; i < value.length; i++){
+      let index = value[i];
+      console.log("this is now in array")
+      if(typeof index === "number" || typeof index === "boolean"){
+         arr.push(index);
+      } else if(typeof index === null || typeof index === undefined){
+        arr.push(stringifyJSON(index))
+      }
+    }
+    return '"' + arr + '"';
+  }
+    // if not then loop through array to get each individual values
+    // .
+    // .
+    // . 
+  // check if is object
+  // if(typeof value === "object"){
+    // check if object is empty
+    // .
+    // .
+    // .
+
 };
